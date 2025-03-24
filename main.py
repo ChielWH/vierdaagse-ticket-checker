6c5142e434a24007374c085c285d4f45a58aa97e
Chiel Heldoorn <chiel.heldoorn@edsn.nl>
2025-03-24 09:04:00 +0100

    Do not open browser when ticket is reserved

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 
+def get_element_by_xpath(driver: webdriver.Firefox, xpath: str):
+    return WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located(
+            (
+                By.XPATH,
+                xpath,
+            )
+        )
+    )
+
+
 def check_tickets():
     ticket_page = "https://atleta.cc/e/zRLhtOq7pOcB/resale"
 
@@ -20,23 +31,28 @@ def check_tickets():
     ) as driver:
         driver.get(ticket_page)
         try:
-            tickets = WebDriverWait(driver, 10).until(
-                EC.visibility_of_element_located(
-                    (
-                        By.XPATH,
-                        "/html/body/div/div/div[2]/div[2]/div/div/div[1]/h5/span[1]",
-                    )
-                )
+            tickets = get_element_by_xpath(
+                driver=driver,
+                xpath="/html/body/div/div/div[2]/div[2]/div/div/div[1]/h5/span[1]",
             )
             n_tickets = tickets.text.split(" ")[0]
-            print(
-                f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] {n_tickets} available"
-            )
 
             if int(n_tickets) > 0:
-                subprocess.run(["open", ticket_page])
-                subprocess.run(
-                    ["open", "https://forms.4daagse.nl/registratienummer-vergeten"]
+                reserved_or_buy_button = get_element_by_xpath(
+                    driver=driver,
+                    xpath="/html/body/div/div/div[2]/div[2]/div/div/div[2]/div/div/div/div[2]/button",
+                )
+                print(
+                    f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] {n_tickets} available, {reserved_or_buy_button.text}"
+                )
+                if reserved_or_buy_button.text != "Reserved":
+                    subprocess.run(["open", ticket_page])
+                    subprocess.run(
+                        ["open", "https://forms.4daagse.nl/registratienummer-vergeten"]
+                    )
+            else:
+                print(
+                    f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] {n_tickets} available"
                 )
         except Exception as e:
             print(
